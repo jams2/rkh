22b58d84b7a27c67ab89c3f0d165deb3341c427e
jams2 <public@elysee-munn.family>
2020-08-30 12:29:14 +0100

    adds content/title word exclusion

--- home/models.py
+++ home/models.py
@@ -1,8 +1,13 @@
+from django.forms.utils import ErrorList
+from django.core.validators import ValidationError
 from wagtail.admin.edit_handlers import StreamFieldPanel
 from wagtail.core import blocks
 from wagtail.core.models import Page
 from wagtail.core.fields import StreamField
 from wagtail.images.blocks import ImageChooserBlock
+
+
+TITLE_BLACKLIST_WORDS = ("the",)
 
 
 class HomePage(Page):
@@ -26,15 +31,55 @@ class LinkButtonBlock(blocks.StructBlock):
     page = blocks.PageChooserBlock(required=False)
     external_url = blocks.URLBlock(required=False)
 
+    def clean(self, value):
+        errors = {}
+        error_message = "exactly one of either page or external_url is required"
+        if (not value.get("page") and not value.get("external_url")) or (
+            value.get("page") and value.get("external_url")
+        ):
+            errors["page"] = ErrorList([error_message])
+            errors["external_url"] = ErrorList([error_message])
+        if errors:
+            raise ValidationError("Validation error in LinkButtonBlock", params=errors)
+        return super().clean(value)
+
     class Meta:
         value_class = LinkButtonValue
 
 
+def title_field_validator(val: str) -> None:
+    for word in TITLE_BLACKLIST_WORDS:
+        if word.lower() in val.lower():
+            raise ValidationError('title cannot contain the word "the"')
+
+
+def trim_trailing_punct(word: str) -> str:
+    return word if word[-1].isalnum() else word[:-1]
+
+
 class RKHStructBlock(blocks.StructBlock):
-    title = blocks.CharBlock()
-    content = blocks.RichTextBlock()
-    link_1 = LinkButtonBlock(required=True)
-    link_2 = LinkButtonBlock(required=True)
+    title = blocks.CharBlock(validators=(title_field_validator,))
+    content = blocks.RichTextBlock(features=["ol", "bold"])
+    link_1 = LinkButtonBlock()
+    link_2 = LinkButtonBlock()
+
+    def clean(self, value):
+        errors = {}
+        exclude_words = set(
+            trim_trailing_punct(x.lower()) for x in value.get("title").split()
+        )
+        content = value.get("content").source.lower()
+        for word in exclude_words:
+            if word in content:
+                errors["content"] = ErrorList(
+                    [f'the word "{word}" cannot be in title AND content']
+                )
+                break
+        if errors:
+            raise ValidationError(
+                "Words in title must not be in content", params=errors
+            )
+        return super().clean(value)
 
 
 class FancyPage(Page):
